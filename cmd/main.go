95f48e6ba64cbe93e7c253405fe28e599159f154
yrs147 <yashraj14700728@gmail.com>
2023-02-18 11:16:28 +0530

    Fixed Errors

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/yrs147/go-book-management-system/pkg/routes"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/",r)
-	log.Fatal(http.ListenAndServe("localhost:9010",r))
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
